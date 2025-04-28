bb093ab826a1e764d27dd7311886f97d5bd19f91
charlie <charliejparker05@gmail.com>
2025-04-28 14:37:06 +0100

    Basic map creation and display
    added:
    create_map - generates map of specified size and sets value
    
    display_map - prints any sized map

--- main.c
+++ main.c
@@ -6,55 +6,39 @@
 
 
 void create_map(int x, int y, char map[x][y]) {
-    for (int i = 0; i < x; i++) {
+    for (int i = 0; i < x; i++) { // iterate though x and y of map
         for (int j = 0; j < y; j++) {
             /*if (map[i][j] == ' ') {
                 continue;
             }*/
-            int r = rand() % (100+1);
+            int r = rand() % (100+1); // choose what each space should be - will be replaced with spawning later
             if (r < 90) {
-                map[i][j] = 'X';
+                map[i][j] = ' ';
             } else if (r < 95) {
                 map[i][j] = '0';
             } else if (r < 100) {
-                map[i][j] = 'S';
+                map[i][j] = 'X';
             } else {
-                map[i][j] = 219;
-            }
-        }
-    }
-    for (int i = 0; i < x; i++) {
-        for (int j = 0; j < y; j++) {
-            if (map[i][j] == '0') {
-                int r = rand() % (4);
-
-                if (r == r && i + 2 < x) {
-                    map[i+1][j] = '|';
-                    map[i+2][j] = '|';
-                }
-
-                //if (r == 1 && j + 2 < y) map[i][j+1] = '-'; map[i][j+2] = '-';
-                //if (r == 2 && i - 1 >=0) map[i-1][j] = '|'; map[i-1][j] = '|';
-                //if (r == 3 && j - 2 >=0) map[i][j-1] = '-'; map[i][j-2] = '-';
+                map[i][j] = 219; // player character, currently just a small chance it shows, to test display
             }
         }
     }
 }
 
 void display_map(int x, int y, char map[x][y]) {
-    for (int i = 0; i < x; i++) {
+    for (int i = 0; i < x; i++) { // iterate through
         for (int j = 0; j < y; j++) {
-            printf("%c  ", map[i][j]);
+            printf("%c  ", map[i][j]); // print with type character
         }
-        printf("\n");
+        printf("\n"); // new line at end of line
     }
 }
 
 main(void) {
-    srand((unsigned)time(NULL));
-    int x = 20, y = 20;
-    char map[x][y];
-    create_map(x, y, map);
-    display_map(x, y, map);
+    srand((unsigned)time(NULL)); // allows for random to be used
+    int x = 20, y = 20; // map size
+    char map[x][y]; // map
+    create_map(x, y, map); // create map
+    display_map(x, y, map); // show map
     return 0;
 }
